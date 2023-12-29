534fde117f3fd3794dc9c54fb3dafa1873fe984e
Shumon Khan <backlink.shumonkhan@gmail.com>
2023-12-30 00:07:34 +0600

    Added email recive functionality by emailjs in .env file use must VITE_THEN_FILE_NAME to use defaul formate

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,112 @@
+import { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+
 export const Contact = () => {
-  return <div>Contact</div>;
+  const formRef = useRef(null);
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({ name: '', email: '', message: '' });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: 'Backlink Shumon',
+          from_email: form.email,
+          to_email: 'backlink.shumonkhan@gmail.com',
+          message: form.message,
+        },
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log('Contact form email error', error);
+      });
+  };
+
+  //click-on the input field
+  const handleFocus = () => {};
+
+  //click out from the input field
+  const handleBlur = () => {};
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col gap-7 mt-14"
+        >
+          <label className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="John"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="John@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              rows="4"
+              className="textarea"
+              placeholder="Let me know how can I help you."
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type="submit"
+            disabled={loading}
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {loading ? 'Sending...' : 'Submit'}
+          </button>
+        </form>
+      </div>
+
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]"></div>
+    </section>
+  );
 };
 
 export default Contact;
